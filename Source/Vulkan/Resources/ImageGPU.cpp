4ef02a935746408a8475039c4d8e4b950c78e7a9
John <goldnotch@mail.ru>
2024-11-29 20:26:48 +0200

    fix some mistakes in VulkanValidationLayer

--- Source/Vulkan/Resources/ImageGPU.cpp
+++ Source/Vulkan/Resources/ImageGPU.cpp
@@ -158,7 +158,34 @@ void ImageGPU::SetImageLayout(details::CommandBuffer & commandBuffer,
   barrier.subresourceRange.layerCount = 1;
   barrier.srcAccessMask = 0; // TODO
   barrier.dstAccessMask = 0; // TODO
-  commandBuffer.PushCommand(vkCmdPipelineBarrier, 0, 0, 0, 0, nullptr, 0, nullptr, 1, &barrier);
+
+  VkPipelineStageFlags sourceStage;
+  VkPipelineStageFlags destinationStage;
+
+  if (m_layout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+  {
+    barrier.srcAccessMask = 0;
+    barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+    sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+    destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+  }
+  else if (m_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL &&
+           newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+  {
+    barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+    sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+    destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+  }
+  else
+  {
+ 
+  }
+
+  commandBuffer.PushCommand(vkCmdPipelineBarrier, sourceStage, destinationStage, 0, 0, nullptr, 0,
+                            nullptr, 1, &barrier);
   m_layout = newLayout;
 }
 
