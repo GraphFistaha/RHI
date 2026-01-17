fe10faa1f0a93d3dcd98e0c5b07187b670a71702
goldnotch <goldnotch@mail.ru>
2026-01-17 08:29:17 +0500

    fix ubuntu

--- install_linux.sh
+++ install_linux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt update
 # install system packages
 sudo apt-get install libgl-dev libgl1-mesa-dev \
 libx11-xcb-dev libfontenc-dev libxaw7-dev libxcomposite-dev libxcursor-dev libxdamage-dev libxext-dev libxfixes-dev libxi-dev \
@@ -9,9 +10,3 @@ uuid-dev libxcb-cursor-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev 
 
 #install vulkan libraries
 sudo apt-get install libvulkan-dev
-
-./Examples/precompile_shaders.sh -o "Build/Debug/Shaders"
-
-echo "All packages were installed. You can configure project"
-echo "Press any button to continue..."
-read x
